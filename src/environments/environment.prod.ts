4473546219cbb05ff95f5d047256959cf3dcd1d8
alvesmwanakunda <thecal4@gmail.com>
2023-02-22 14:34:42 +0000

    Gestion user

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  //BASE_API_URL: "https://wefid.alwaysdata.net/wefid"
-  BASE_API_URL: "https://guarded-sea-81297.herokuapp.com"
+  BASE_API_URL: "https://wefid.io/api"
+  //BASE_API_URL: "https://guarded-sea-81297.herokuapp.com"
 
 };
